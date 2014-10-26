72813803e27a1d27251e43e45313d2fae87861a7
alexandre sroussi <alex@alexandres-iMac.local>
2014-10-26 10:19:46 +0200

    cachematrix methods

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix - wrapper to cache matrix. provides methods to set/get the initial matrix, and cache/retrieve its inverse
+## cacheSolve - replacement method for R solve method. takes as argument the output of makeCacheMatrix (instead of regular matrix). caches the inverse matrix.
 
-## Write a short comment describing this function
-
+## wrapper for matric caching. the input is the initial matrix. $set replace the matric (and evicts the cached inverse - if any). $setsolve and $getsolve caches and retrieves the inverse matrix of the current input matrix
 makeCacheMatrix <- function(x = matrix()) {
 	s <- NULL
     
 	set <- function(y) {
         x <<- y
+        ## evict cache
         s <<- NULL
     }
     
@@ -23,9 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## resolves inverse matrix eighter b calculating or resolving the cached result
 cacheSolve <- function(x, ...) {
+ 	## try cache
     s <- x$getsolve()
     if(!is.null(s)) {
         message("getting cached data")
